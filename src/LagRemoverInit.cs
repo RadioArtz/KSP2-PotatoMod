d2e28241a325ffab47211eaaeb5160a527c3807e
RadioArtz <54477532+RadioArtz@users.noreply.github.com>
2023-02-25 10:26:45 +0100

    Various Improvements (read desc)
    -more things get disabled / removed now: mainly water though (THIS NO LONGER HAS PHYSICS COLLISION!!)
    -fixed lots of objects not getting disabled as intended
    -clouds are now fully gone and no longer spam the logs with errors

--- src/LagRemoverInit.cs
+++ src/LagRemoverInit.cs
@@ -1,12 +1,14 @@
 ﻿using SpaceWarp.API;
 using UnityEngine;
 using KSP.Game;
+using System.Reflection;
+
 namespace LagRemover
 {
     [MainMod]
     public class LagRemoverInit : Mod
     {
-        string[] removeList = {"grass","light","cloud","tree", "emissive","bush","decals"};
+        string[] removeList = {"grass","light","tree", "emissive","bush","decal","yield_sign","stop_sign","hvac",};
         public override void Initialize()
         {
             Logger.Info("Mod is initialized");
@@ -16,11 +18,14 @@ namespace LagRemover
         {
             if (Input.GetKeyDown("space"))
             {
+                Time.fixedDeltaTime = 0.035f;
                 QualitySettings.shadows = ShadowQuality.Disable;
                 QualitySettings.realtimeReflectionProbes = false;
+                RenderSettings.defaultReflectionMode = 0;
+                RenderSettings.defaultReflectionResolution = 0;
+
                 QualitySettings.antiAliasing = 0;
                 QualitySettings.softParticles = false;
-                Time.fixedDeltaTime = 0.035f;
                 QualitySettings.maximumLODLevel = 4;
                 QualitySettings.pixelLightCount = 1;
                 QualitySettings.masterTextureLimit = 64;
@@ -29,19 +34,31 @@ namespace LagRemover
                 pqs.isSubdivisionEnabled = false;
                 KSP.Rendering.Planets.AtmosphereScatterManager atmomanager = FindObjectOfType<KSP.Rendering.Planets.AtmosphereScatterManager>();
                 Destroy(atmomanager);
-                pqs.settings.subdivisionInfo.subdivData.maxLevel = 2;
+                pqs.settings.subdivisionInfo.subdivData.maxLevel = 0;
+                pqs.PQSRenderer.MaxSubdivision = 0;
+                pqs.PQSRenderer.RenderDepthTexture = false;
+                pqs.PQSRenderer.Layer = 0;
                 pqs.PQSRenderer.OceanQualitySetting = 0;
                 pqs.PQSRenderer.EnableLowQualityLocal = true;
+
+                KSP.VolumeCloud.VolumeCloudRenderer[] clouds = FindObjectsOfType<KSP.VolumeCloud.VolumeCloudRenderer>();
+
+                foreach(KSP.VolumeCloud.VolumeCloudRenderer cloud in clouds)
+                {
+                    cloud.enabled = false;
+                }
+
                 KSP.Rendering.PostProcessingSystem ppsys = FindObjectOfType<KSP.Rendering.PostProcessingSystem>();
                 Destroy(ppsys);
-                Camera[] cams = Camera.allCameras;
-                foreach (Camera cam in cams)
-                {
-                    if(cam.renderingPath == RenderingPath.DeferredLighting || cam.renderingPath == RenderingPath.DeferredShading)
-                    {
-                        cam.renderingPath = RenderingPath.VertexLit;
-                    }
-                }
+
+                //thanks Plexus du Menton for this ^^
+                FieldInfo oceanSphereMaterialField = typeof(KSP.Rendering.Planets.PQSRenderer).GetField("_oceanSpereMaterial", BindingFlags.NonPublic | BindingFlags.Instance);
+                FieldInfo oceanMaterialField = typeof(KSP.Rendering.Planets.PQSRenderer).GetField("_oceanMaterial", BindingFlags.NonPublic | BindingFlags.Instance);
+                FieldInfo oceanBackMaterialField = typeof(KSP.Rendering.Planets.PQSRenderer).GetField("_oceanBackSurfaceMaterial", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                oceanSphereMaterialField.SetValue(pqs.PQSRenderer, null);
+                oceanMaterialField.SetValue(pqs.PQSRenderer, null);
+                oceanBackMaterialField.SetValue(pqs.PQSRenderer, null);
 
                 GameObject[] gos = FindObjectsOfType<GameObject>();
                 foreach (GameObject go in gos)
@@ -50,11 +67,32 @@ namespace LagRemover
                     {
                         foreach(string str in removeList)
                         {
-                            if (go.name.Contains(str))
-                                Destroy(go);
+                            if (go.name.ToLower().Contains(str))
+                            {
+                                DestroyImmediate(go);
+                                break;
+                            }
+                                
                         }
                     }
                 }
+                //This is for debugging purposes
+                /*
+                gos = FindObjectsOfType<GameObject>();
+                foreach (GameObject go in gos)
+                {
+                    if (go.GetComponent<MeshRenderer>() != null)
+                    {
+                        foreach (string str in removeList)
+                        {
+                            if (!go.name.Contains(str))
+                            {
+                                Logger.Info(go.name);
+                                break;
+                            }    
+                        }
+                    }
+                }*/
             }
         }
     }
