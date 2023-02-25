8df18d0d06ffb369e09fe86d5d9326837a0373c5
RadioArtz <54477532+RadioArtz@users.noreply.github.com>
2023-02-25 18:38:28 +0100

    add a hacky improvement to JOINT_RIGIDITY,  change keybinds
    increasing JointRigidity should help with craft stability and hopefully some performance aswell

--- src/LagRemoverInit.cs
+++ src/LagRemoverInit.cs
@@ -2,28 +2,32 @@
 using UnityEngine;
 using KSP.Game;
 using System.Reflection;
+using KSP.IO;
 
 namespace LagRemover
 {
     [MainMod]
     public class LagRemoverInit : Mod
     {
-        string[] removeList = {"grass","light","tree", "emissive","bush","decal","yield_sign","stop_sign","hvac",};
+        string[] removeList = { "grass", "light", "tree", "emissive", "bush", "decal", "sign", "hvac", "lamp"};
         public override void Initialize()
         {
+            UpdatePhysSettings();
             Logger.Info("Mod is initialized");
         }
 
         public void Update()
         {
-            if (Input.GetKeyDown("space"))
+            if (Input.GetKeyDown(KeyCode.P) && Input.GetKey(KeyCode.LeftControl))
             {
+                Logger.Info("Loaded JOINT_RIGIDITY with value of" + PhysicsSettings.JOINT_RIGIDITY);
+
                 Time.fixedDeltaTime = 0.035f;
                 QualitySettings.shadows = ShadowQuality.Disable;
                 QualitySettings.realtimeReflectionProbes = false;
                 RenderSettings.defaultReflectionMode = 0;
                 RenderSettings.defaultReflectionResolution = 0;
-
+                
                 QualitySettings.antiAliasing = 0;
                 QualitySettings.softParticles = false;
                 QualitySettings.maximumLODLevel = 4;
@@ -32,6 +36,12 @@ namespace LagRemover
                 KSP.Rendering.Planets.PQS pqs = FindObjectOfType<KSP.Rendering.Planets.PQS>();
                 pqs.settings.colliderCullingDistance = 2000;
                 pqs.isSubdivisionEnabled = false;
+                KSP.Rendering.CelestialBodyGIProbeManager CBGIPmgr = FindObjectOfType<KSP.Rendering.CelestialBodyGIProbeManager>();
+                CBGIPmgr.enableGlobalIllumination = false;
+                CBGIPmgr.enableCelestialBodyGI = false;
+                CBGIPmgr.enableObserverGI = false;
+
+
                 KSP.Rendering.Planets.AtmosphereScatterManager atmomanager = FindObjectOfType<KSP.Rendering.Planets.AtmosphereScatterManager>();
                 Destroy(atmomanager);
                 pqs.settings.subdivisionInfo.subdivData.maxLevel = 0;
@@ -40,16 +50,17 @@ namespace LagRemover
                 pqs.PQSRenderer.Layer = 0;
                 pqs.PQSRenderer.OceanQualitySetting = 0;
                 pqs.PQSRenderer.EnableLowQualityLocal = true;
+                
 
                 KSP.VolumeCloud.VolumeCloudRenderer[] clouds = FindObjectsOfType<KSP.VolumeCloud.VolumeCloudRenderer>();
 
-                foreach(KSP.VolumeCloud.VolumeCloudRenderer cloud in clouds)
+                foreach (KSP.VolumeCloud.VolumeCloudRenderer cloud in clouds)
                 {
                     cloud.enabled = false;
                 }
 
                 KSP.Rendering.PostProcessingSystem ppsys = FindObjectOfType<KSP.Rendering.PostProcessingSystem>();
-                Destroy(ppsys);
+                ppsys.enabled = false;
 
                 //thanks Plexus du Menton for this ^^
                 FieldInfo oceanSphereMaterialField = typeof(KSP.Rendering.Planets.PQSRenderer).GetField("_oceanSpereMaterial", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -65,16 +76,20 @@ namespace LagRemover
                 {
                     if (go.GetComponent<MeshRenderer>() != null)
                     {
-                        foreach(string str in removeList)
+                        foreach (string str in removeList)
                         {
                             if (go.name.ToLower().Contains(str))
                             {
                                 DestroyImmediate(go);
                                 break;
                             }
-                                
+
                         }
-                    }
+                    }/*
+                    if (go.GetComponent<Light>())
+                    {
+                        DestroyImmediate(go.GetComponent<Light>());
+                    }*/
                 }
                 //This is for debugging purposes
                 /*
@@ -94,6 +109,26 @@ namespace LagRemover
                     }
                 }*/
             }
+        }
+
+        /// <summary>
+        /// this forces the Joint Rigidity to use a high value. this helps stabilize rockets and may even increase performance with wobbly rockets.
+        /// </summary>
+        void UpdatePhysSettings()
+        {
+            bool _physicsSettingsFileExists = IOProvider.JsonFileExists(IOProvider.DataLocation.Global, "PhysicsSettings");
+            Logger.Error("No phyfile exists");
+            string filepath = IOProvider.JoinPath(IOProvider.PathOfDataType(IOProvider.DataLocation.Global), "PhysicsSettings") + ".json";
+            string loadedFile = IOProvider.ReadAllText(filepath);
+
+            int targetJointRigidity = 1500000;
+
+            int jointrigidityStartIndex = loadedFile.IndexOf("\"JOINT_RIGIDITY\": ");
+            string tmpsubstring = loadedFile.Substring(jointrigidityStartIndex, (loadedFile.IndexOf(",", jointrigidityStartIndex)) - jointrigidityStartIndex);
+            loadedFile = loadedFile.Replace(tmpsubstring, "\"JOINT_RIGIDITY\": " + targetJointRigidity + ".0");
+
+            IOProvider.WriteAllText(filepath, loadedFile);
+            bool physSettingsLoaded = PhysicsSettingsManager.IsPhysicsSettingsLoaded;
         }
     }
 }
