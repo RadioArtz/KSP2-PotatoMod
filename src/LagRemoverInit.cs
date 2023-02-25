f7a29243b11cac5d6042bb86cea98ce5ee9ba5a1
RadioArtz <54477532+RadioArtz@users.noreply.github.com>
2023-02-25 08:02:34 +0100

    init repo with source and binary

---
+++ src/LagRemoverInit.cs
@@ -0,0 +1,61 @@
+﻿using SpaceWarp.API;
+using UnityEngine;
+using KSP.Game;
+namespace LagRemover
+{
+    [MainMod]
+    public class LagRemoverInit : Mod
+    {
+        string[] removeList = {"grass","light","cloud","tree", "emissive","bush","decals"};
+        public override void Initialize()
+        {
+            Logger.Info("Mod is initialized");
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                QualitySettings.shadows = ShadowQuality.Disable;
+                QualitySettings.realtimeReflectionProbes = false;
+                QualitySettings.antiAliasing = 0;
+                QualitySettings.softParticles = false;
+                Time.fixedDeltaTime = 0.035f;
+                QualitySettings.maximumLODLevel = 4;
+                QualitySettings.pixelLightCount = 1;
+                QualitySettings.masterTextureLimit = 64;
+                KSP.Rendering.Planets.PQS pqs = FindObjectOfType<KSP.Rendering.Planets.PQS>();
+                pqs.settings.colliderCullingDistance = 2000;
+                pqs.isSubdivisionEnabled = false;
+                KSP.Rendering.Planets.AtmosphereScatterManager atmomanager = FindObjectOfType<KSP.Rendering.Planets.AtmosphereScatterManager>();
+                Destroy(atmomanager);
+                pqs.settings.subdivisionInfo.subdivData.maxLevel = 2;
+                pqs.PQSRenderer.OceanQualitySetting = 0;
+                pqs.PQSRenderer.EnableLowQualityLocal = true;
+                KSP.Rendering.PostProcessingSystem ppsys = FindObjectOfType<KSP.Rendering.PostProcessingSystem>();
+                Destroy(ppsys);
+                Camera[] cams = Camera.allCameras;
+                foreach (Camera cam in cams)
+                {
+                    if(cam.renderingPath == RenderingPath.DeferredLighting || cam.renderingPath == RenderingPath.DeferredShading)
+                    {
+                        cam.renderingPath = RenderingPath.VertexLit;
+                    }
+                }
+
+                GameObject[] gos = FindObjectsOfType<GameObject>();
+                foreach (GameObject go in gos)
+                {
+                    if (go.GetComponent<MeshRenderer>() != null)
+                    {
+                        foreach(string str in removeList)
+                        {
+                            if (go.name.Contains(str))
+                                Destroy(go);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
